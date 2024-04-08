c091edcd9c9f0f4507e78c9e0624d0bf4e8646e4
mheathco <mheathco@ucsc.edu>
2024-04-08 02:59:25 -0700

    can draw vectors with user input

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -16,8 +16,12 @@ function main() {
   }
 
   // Clear canvas to black 
-  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
-  ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
+  function clearCanvas() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle
+  }
+
+  clearCanvas();
 
   // Function to draw vector
   function drawVector(v, color) {
@@ -30,12 +34,45 @@ function main() {
 
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
-    ctx.lineTo(centerX + (v1.elements[0] * 20), centerY - (v1.elements[1] * 20));
+    ctx.lineTo(centerX + (v.elements[0] * 20), centerY - (v.elements[1] * 20));
     ctx.stroke();
   }
 
+  // Get input variables
+  var v1x = document.getElementById("v1_x");
+  var v1y = document.getElementById("v1_y");
+  var v2x = document.getElementById("v2_x");
+  var v2y = document.getElementById("v2_y");
+  
+  // Handle user clicking "Draw"
+  let button = document.getElementById("button");
+  console.log(button);
+  
+  button.onclick = function() {
+    handleDrawEvent();
+  }
+
+  function handleDrawEvent(v1, v2) {
+    console.log("clicked!");
+    var v1x_val = v1x.value;
+    var v1y_val = v1y.value;
+    var v2x_val = v2x.value;
+    var v2y_val = v2y.value;
+    console.log("v1 = (", v1x_val, ", ", v1y_val, ")");
+    console.log("v2 = (", v2x_val, ", ", v2y_val, ")");
+    
+    // draw v1 and v2
+    clearCanvas();
+    var v1 = new Vector3([v1x_val, v1y_val, 0]);
+    console.log("v1 = ", v1);
+    drawVector(v1, "red");
+    var v2 = new Vector3([v2x_val, v2y_val, 0]);
+    console.log("v2 = ", v2);
+    drawVector(v2, "red");
+  }
+
   // Draw a red vector v1
-  var v1 = new Vector3([2.25, 2.25, 0]);
-  drawVector(v1, "red");
+  //var v1 = new Vector3([2.25, 2.25, 0]);
+  //drawVector(v1, "red");
 
 }
