65183d448b051628a68fe29cd77c062c709e842c
mheathco <mheathco@ucsc.edu>
2024-04-11 21:51:27 -0700

    added point class

--- asgn1/src/ColoredPoints.js
+++ asgn1/src/ColoredPoints.js
@@ -117,23 +117,43 @@ function main() {
 
 
 // Globals for shape arrays
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-var g_sizes = [];   // The array to store the size of a point
+//var g_points = [];  // The array for the position of a mouse press
+//var g_colors = [];  // The array to store the color of a point
+//var g_sizes = [];   // The array to store the size of a point
+
+class Point {
+  constructor() {
+    this.type='point';
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.size = 5.0;
+  }
+}
+
+var g_shapesList = [];
 
 // ----- click -----
 function click(ev) {
   // Extract the event click and return it in WebGL coordinates
   [x, y] = convertCoordinatesEventToGL(ev);
 
+  // Create the new point
+  let point = new Point();
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();  // without ".slice()", it pushes a pointer
+  point.size = g_selectedSize;
+
+  // Store the new point
+  g_shapesList.push(point);
+
   // Store the coordinates to g_points array
-  g_points.push([x, y]);
+  //g_points.push([x, y]);
 
   // Store the color to g_colors array
-  g_colors.push(g_selectedColor.slice());  // without ".slice()", it pushes a pointer
+  //g_colors.push(g_selectedColor.slice());  // without ".slice()", it pushes a pointer
   
   // Store the size to the g_sizes array
-  g_sizes.push(g_selectedSize);
+  //g_sizes.push(g_selectedSize);
 
   // Draw every shape that is supposed to be in the canvas
   renderAllShapes();
@@ -145,11 +165,16 @@ function renderAllShapes(){
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  //var len = g_points.length;
+  var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-    var size = g_sizes[i];
+    //var xy = g_points[i];
+    //var rgba = g_colors[i];
+    //var size = g_sizes[i];
+    
+    var xy = g_shapesList[i].position;
+    var rgba = g_shapesList[i].color;
+    var size = g_shapesList[i].size;
 
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
