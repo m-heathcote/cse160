a75517de2da8a58530a0aab4d502ced0e82eaf59
mheathco <mheathco@ucsc.edu>
2024-04-20 19:06:34 -0700

    adding comments

--- asgn5/src/threejs.js
+++ asgn5/src/threejs.js
@@ -2,30 +2,84 @@ import * as THREE from 'three';
 
 function main() {
 
-	const canvas = document.querySelector( '#c' );
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+  // look up the canvas
+	const canvas = document.querySelector('#c');
+	
+  // create a renderer
+  const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
-	const fov = 75;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+  // create a camera
+	const fov = 75; // 75 degrees (vertical direction)
+	const aspect = 2; // the canvas default (300/150)
+	const near = 0.1; // anything before near won't be rendered
+	const far = 5; // anything after far won't be rendered
+	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.z = 2; // looking down the z-axis towards -z
 
+  // create a scene
 	const scene = new THREE.Scene();
+	{
+    // create a directional light
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight(color, intensity);
+		light.position.set(-1, 2, 4);
+		scene.add(light);
+	}
 
+  // create a box
 	const boxWidth = 1;
 	const boxHeight = 1;
 	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-	const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
+	function makeInstance(geometry, color, x) {
+    // create a basic material and set its color
+		const material = new THREE.MeshPhongMaterial({color});
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+    // create a mesh
+    // (combines: geometry, material, and position/orientation/scale)
+		const cube = new THREE.Mesh(geometry, material);
+		
+    // add mesh (cube) to scene
+    scene.add(cube);
 
-	renderer.render( scene, camera );
+		cube.position.x = x;
+
+		return cube;
+	}
+
+  // 3 cubes
+	const cubes = [
+		makeInstance( geometry, 0x44aa88, 0 ),
+		makeInstance( geometry, 0x8844aa, -2 ),
+		makeInstance( geometry, 0xaa8844, 2 ),
+	];
+
+	function render(time) {
+		time *= 0.001; // convert time to seconds
+
+		cubes.forEach((cube, ndx) => {
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+
+      // set rotations (in radians)
+      // 2pi radians in 1 circle
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
+		});
+
+    // render the scene
+		renderer.render(scene, camera);
+
+		requestAnimationFrame(render);
+	}
+
+  // animate
+  // (passes the time since the page loaded into the function render)
+	requestAnimationFrame(render);
 
 }
 
 main();
+
