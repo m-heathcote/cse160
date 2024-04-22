13b1edd924878650146771a8908c828d1dfd5c14
mheathco <mheathco@ucsc.edu>
2024-04-21 17:38:02 -0700

    starting actual project

--- asgn5/src/threejs.js
+++ asgn5/src/threejs.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 
 function main() {
 
@@ -9,84 +12,124 @@ function main() {
   const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
   // create a camera
-	const fov = 75; // 75 degrees (vertical direction)
-	const aspect = 2; // the canvas default (300/150)
-	const near = 0.1; // anything before near won't be rendered
-	const far = 5; // anything after far won't be rendered
+	const fov = 45;
+	const aspect = 2; // the canvas default
+	const near = 0.1;
+	const far = 100;
 	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	camera.position.z = 2; // looking down the z-axis towards -z
+	camera.position.set(0, 10, 25);
+
+  // orbit controls
+	const controls = new OrbitControls(camera, canvas);
+	controls.target.set(0, 5, 0);
+	controls.update();
 
   // create a scene
 	const scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xADD9FF);
+	
+  // -- Checkered Empty Floor --
+  {
+		const planeSize = 50;
+
+    // texture loader
+		const loader = new THREE.TextureLoader();
+		const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+		texture.colorSpace = THREE.SRGBColorSpace;
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.magFilter = THREE.NearestFilter;
+		const repeats = planeSize / 2;
+		texture.repeat.set(repeats, repeats);
+
+    // create plane and add texture
+		const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+		const planeMat = new THREE.MeshPhongMaterial({
+			map: texture,
+			side: THREE.DoubleSide,
+		});
+
+    // mesh
+		const mesh = new THREE.Mesh(planeGeo, planeMat);
+		mesh.rotation.x = Math.PI * - .5;
+		scene.add(mesh);
+	}
+  // -- (end Checkered Empty Floor) --
+
+  // -- Hemisphere Light --
 	{
-    // create a directional light
+		const skyColor = 0xC4E4FF; // light blue
+		//const groundColor = 0xB97A20; // brownish orange
+		const groundColor = 0x596090; // grayish purple
+		const intensity = 3;
+		const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+		scene.add(light);
+	}
+  // -- (end Hemisphere Light) --
+
+
+  // -- Directional Light --
+	{
 		const color = 0xFFFFFF;
 		const intensity = 3;
 		const light = new THREE.DirectionalLight(color, intensity);
-		
-    // default position and target = (0, 0, 0)
-    light.position.set(-1, 2, 4);
-		
-    // add to scene
-    scene.add(light);
+		light.position.set(5, 10, 2);
+		scene.add(light);
+		scene.add(light.target);
 	}
+  // -- (end Directional Light) --
 
-  // create a box
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  // -- Chair --
+	{
+		const mtlLoader = new MTLLoader();
+		mtlLoader.load('../objs/Palm/materials.mtl', (mtl) => {
 
-	function makeInstance(geometry, color, x) {
-    // create a material and set its color
-		// MeshBasicMaterial is NOT affected by lights
-    // MeshPhongMaterial IS affected by lights
-    const material = new THREE.MeshPhongMaterial({color});
+			mtl.preload();
+			const objLoader = new OBJLoader();
+			//mtl.materials.Material.side = THREE.DoubleSide;
+			objLoader.setMaterials(mtl);
+			objLoader.load('../objs/Palm/model.obj', (root) => {
+        // scale
+        const scaleFactor = 10;
+        root.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
-    // create a mesh
-    // (combines: geometry, material, and position/orientation/scale)
-		const cube = new THREE.Mesh(geometry, material);
-		
-    // add mesh (cube) to scene
-    scene.add(cube);
+        // move
+        const newPosition = new THREE.Vector3(0, 2.8, 0);
+        root.position.copy(newPosition);
 
-		cube.position.x = x;
-		cube.position.z = -0.5;
-
-		return cube;
-	}
-
-  // 3 cubes
-	const cubes = [
-		makeInstance( geometry, 0xFF75CF, 0 ),
-		makeInstance( geometry, 0xFFDC54, -2 ),
-		makeInstance( geometry, 0x54DCFF, 2 ),
-	];
-
-	function render(time) {
-		time *= 0.001; // convert time to seconds
-
-		cubes.forEach((cube, ndx) => {
-			const speed = 1 + ndx * .1;
-			const rot = time * speed;
-
-      // set rotations (in radians)
-      // 2pi radians in 1 circle
-			cube.rotation.x = rot;
-			cube.rotation.y = rot;
+        // add to scene
+				scene.add(root);
+			});
 		});
+	}
+  // -- (end Chair) --
 
-    // render the scene
+	function resizeRendererToDisplaySize(renderer) {
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if (needResize) {
+			renderer.setSize(width, height, false);
+		}
+
+		return needResize;
+	}
+
+	function render() {
+		if (resizeRendererToDisplaySize(renderer)) {
+
+			const canvas = renderer.domElement;
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
+		}
+
 		renderer.render(scene, camera);
 
-    // animate
 		requestAnimationFrame(render);
 	}
 
-  // start animation
-  // (passes the time since the page loaded into the function render)
 	requestAnimationFrame(render);
-
 }
 
 main();
